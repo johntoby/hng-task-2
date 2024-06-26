f47977d22e9c9a32017d10e70239aadd8f43af2a
johntoby <johntoby007@gmail.com>
2024-06-26 07:07:51 +0100

    my applications files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Generate a random number between 1 and 100
+    random_number = random.randint(1, 100)
+    return render_template('index.html', number=random_number)
+
+if __name__ == '__main__':
+    app.run(debug=True)
